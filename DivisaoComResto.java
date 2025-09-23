5e4c92e6c735be68bffb8a6ee117ad26cd1d30b5
Thaissa Conceição Celestino <pet0144791@aluno.firjansenaisesi.com.br>
2025-09-23 19:51:58 -0300

    Novos exercicios do dia 23

---
+++ DivisaoComResto.java
@@ -0,0 +1,8 @@
+public class DivisaoComResto{
+    public static void main(String[] args){
+        int totalDeIntens=10;
+        int capacidadeDaCaixa= 3;
+       System.out.println("Serao preenchidos "+totalDeIntens/capacidadeDaCaixa+ " caixas.\nSobrarao "+totalDeIntens%capacidadeDaCaixa + " itens.");
+
+    }
+}
